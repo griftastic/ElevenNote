f8acc761845cf233e7ffc2fbbf5763468494931a
Griffin Brown <browgrif@gmail.com>
2022-05-02 11:19:14 -0400

    Get Tokens service method

---
+++ ElevenNote.Services/Token/ITokenService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElevenNote.Models.Token;
+
+namespace ElevenNote.Services.Token
+{
+    public interface ITokenService
+    {
+        public interface ITokenService
+        {
+            Task<TokenResponse> GetTokenAsync(TokenRequest model);
+        }
+    }
+}
